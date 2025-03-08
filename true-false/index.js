7a5272a75908ef294524d2a077955d2345f6c741
Imrul Hassaan Emon <imrulhassan989@gmail.com>
2025-03-09 02:04:39 +0600

    true-false(page-42)

---
+++ true-false/index.js
@@ -0,0 +1,26 @@
+const pillow = 4;
+console.log(pillow)
+
+const bookToRead = 3;
+console.log(bookToRead)
+
+const location = 'andarkilla bandarban'
+console.log(location);
+
+const favFood = 'briyani'
+console.log(favFood);
+
+const name = 'Imrul'
+console.log(name);
+
+const teacher = 'Osthir Chancal'
+console.log(teacher);
+
+const edu = 'Dhaka University'
+console.log(edu);
+
+const fan = 'Pory mony'
+console.log(fan);
+
+const hobby = 'Photography'
+console.log(hobby);
