aae11afd177537cff12301d00268ab4900c3b5a5
Imrul Hassaan Emon <imrulhassan989@gmail.com>
2025-03-10 06:24:47 +0600

    problems -(page-43)

--- true-false/index.js
+++ true-false/index.js
@@ -1,3 +1,4 @@
+// strings problems 
 const pillow = 4;
 console.log(pillow)
 
@@ -24,3 +25,47 @@ console.log(fan);
 
 const hobby = 'Photography'
 console.log(hobby);
+
+// true false problems
+
+const isRich = true;
+const passed = false;
+console.log(isRich)
+console.log(passed);
+
+
+// problems - 01 
+const isbambu = false;
+console.log(isbambu);
+
+// problems - 02
+
+const iscrushedMessaged = false;
+console.log(iscrushedMessaged);
+
+// problems - 03
+
+const celebrating = true;
+console.log(celebrating);
+
+// problems - 04 
+
+const isHappy = true;
+const hasMoney = false;
+const isHungry = true;
+
+console.log(isHappy, hasMoney, isHungry);
+
+// problems - 05 
+
+const isStudyDone = false;
+console.log(isStudyDone);
+
+// problems - 06
+const isPohoneCharged = false;
+console.log(isPohoneCharged);
+
+// problems - 07 
+
+const isDinnerDone = true;
+console.log(isDinnerDone);
