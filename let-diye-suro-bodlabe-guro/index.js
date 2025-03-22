66e25ed6f7f88b53afde1f59decb345d2c95349c
Imrul Hassaan Emon <imrulhassan989@gmail.com>
2025-03-22 16:20:38 +0600

    let-diye-suro-bodlabe-guro

---
+++ let-diye-suro-bodlabe-guro/index.js
@@ -0,0 +1,43 @@
+// problem - 01 
+
+let age = 20;
+console.log(age);
+age = 32;
+console.log(age);
+
+// problem - 02 
+
+let pocketMoney = 500;
+pocketMoney = 150;
+console.log(pocketMoney);
+
+// problem - 03 
+
+let studyTime = 4;
+studyTime = 0;
+console.log(studyTime);
+
+// problem - 04 
+
+let friendsCount = 3;
+friendsCount = friendsCount - 2
+console.log(friendsCount);
+
+// problem - 05 
+
+let phonePrice = 25000;
+phonePrice = 12000;
+console.log(phonePrice);
+
+// problem - 06 
+
+let chairCount = 40;
+chairCount = chairCount - 2;
+chairCount = chairCount - 4;
+console.log(chairCount);
+
+// problem - 07 
+
+let playTime = 3;
+playTime = 0;
+console.log(playTime);
