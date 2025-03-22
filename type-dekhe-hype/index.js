6ca7db4d8ef5fdb1f4935e5e7c85166204ff57ea
Imrul Hassaan Emon <imrulhassan989@gmail.com>
2025-03-22 16:03:35 +0600

    typer dekhe hype

---
+++ type-dekhe-hype/index.js
@@ -0,0 +1,29 @@
+// problem - 01 
+
+const price = 555;
+console.log(typeof price)
+
+// problem - 02 
+
+const learning = true;
+console.log(typeof learning)
+
+// problem - 03
+
+const megaCity = 'Dhaka';
+console.log(typeof megaCity)
+
+// problem -04 
+
+const temperature = 102.5;
+console.log(typeof temperature)
+
+// problems - 05 
+
+const isHungry = true;
+console.log(typeof isHungry);
+
+// problems - 06 
+
+const isRich = true;
+console.log(typeof isRich);
