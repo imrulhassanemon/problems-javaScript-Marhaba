60496ce736523d11b47f3cd1d848271905f910c8
Imrul Hassaan Emon <imrulhassan989@gmail.com>
2025-03-23 11:35:27 +0600

    comment

---
+++ comment/index.js
@@ -0,0 +1,62 @@
+// problem - 01 
+// this is the score of your result 
+const score = 3;
+
+
+
+
+// problem - 02 
+const radius = 3;
+// this is the radious of the circle
+// the circle is made for learnig donout
+// the car is rolling in the circle is calld donout
+
+
+
+// problem - 03 
+const dress = 3; 
+/* 
+    I have not so much dress I have only 3 or 4 shirt 
+    and aslo have 3 or 4 t-shirt 
+    and also have 4-5 full pant 
+    and 3 shorts two of these are useless becase I am not comfortable to ware these 2 shorts
+*/
+
+
+
+
+// problem - 04 
+const giftMoney = 100;
+/* 
+    I got these from my mom for salami.
+*/
+
+
+
+
+// problem - 05 
+const studyTime = 7; 
+/* 
+    I will read and write for 7 hour.
+    2 hour I will be write 
+    2 hour I will be read
+    and 3 hour I will be programming
+*/
+
+
+
+
+// problem - 06 
+
+const sendMoney = 100;
+/* 
+    malek will get 100 taka for me 
+    send money cost will 2 taka for 100 taka
+*/
+
+
+// problem - 07 
+
+// I orderd a book for me from rokomari and I got free shipping
+const pay = 685;
+
